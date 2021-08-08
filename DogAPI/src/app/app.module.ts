f54b7d6af8372d6db3da7fed603655394576b080
Joshua Herold <joshua-herold@gmx.de>
2021-08-08 17:06:24 +0200

    Help-Page erstellt, Design Änderungen

--- DogAPI/src/app/app.module.ts
+++ DogAPI/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms'
 import { IonicStorageModule } from '@ionic/storage-angular';
 
 import { ShareButtonsModule } from 'ngx-sharebuttons/buttons';
